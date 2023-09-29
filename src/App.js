206a7a2fdab10995c4db4b435a47284de674eaca
enginbaris <enginnbaris@gmail.com>
2023-09-29 15:56:59 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {Button,ExampleComponent} from "catui3"
+import './App.css';
+import 'catui3/dist/index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Button text="butona bas!" />
+     
+    </div>
+  );
+}
+
+export default App;
